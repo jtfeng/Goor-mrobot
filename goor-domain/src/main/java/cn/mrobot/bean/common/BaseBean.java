827c44b4bbadbb71a7bea2904567ce7c064cd46b
chay.ni <chay.ni@mrobot.cn>
2017-06-22 17:48:40 +0800

    基类merchantStoreId

--- goor-domain/src/main/java/cn/mrobot/bean/common/BaseBean.java
+++ goor-domain/src/main/java/cn/mrobot/bean/common/BaseBean.java
@@ -4,16 +4,16 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class BaseBean implements Serializable {
-    private Long merchantId;//关联门店ID
+    private Long merchantStoreId;//关联门店ID
     private Long createdBy;//创建用户
     private Date created;//创建时间
 
-    public Long getMerchantId() {
-        return merchantId;
+    public Long getMerchantStoreId() {
+        return merchantStoreId;
     }
 
-    public void setMerchantId(Long merchantId) {
-        this.merchantId = merchantId;
+    public void setMerchantStoreId(Long merchantStoreId) {
+        this.merchantStoreId = merchantStoreId;
     }
 
     public Long getCreatedBy() {
