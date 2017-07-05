7aeb5b5706e2cc074d32e832b487b7d0f4ab9968
ray.fu <ray.fu@mrobot.cn>
2017-07-05 16:47:41 +0800

    打开注释掉的代码

--- goor-server/src/main/java/cn/muye/account/user/controller/UserController.java
+++ goor-server/src/main/java/cn/muye/account/user/controller/UserController.java
@@ -48,14 +48,14 @@ public class UserController {
 
     private static HttpClient httpClient = new HttpClient();
 
-//    @Value("${authServer.host}")
-//    private String authServerHost;
-//
-//    @Value("${authServer.port}")
-//    private String authServerPort;
-//
-//    @Value("${authServer.api}")
-//    private String authServerApi;
+    @Value("${authServer.host}")
+    private String authServerHost;
+
+    @Value("${authServer.port}")
+    private String authServerPort;
+
+    @Value("${authServer.api}")
+    private String authServerApi;
 
     /**
      * 新增修改用户
@@ -299,32 +299,31 @@ public class UserController {
      * @throws Exception
      */
     private String doAuthorize(String username, String pwd) throws Exception {
-//        HttpClientParams httpParams = new HttpClientParams();
-//        httpParams.setSoTimeout(30000);
-//        httpClient.setParams(httpParams);
-//        httpClient.getHostConfiguration().setHost(authServerHost, Integer.valueOf(authServerPort));
-//        httpClient.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "UTF-8");
-//        PostMethod login = new PostMethod(authServerApi);
-//        login.addRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-//        final String auth = "Basic " + new String(Base64.encodeBase64(new StringBuilder(Constant.AUTHORIZE_USERNAME + ":" + Constant.AUTHORIZE_PASSWORD).toString().getBytes()));
-//        login.addRequestHeader("Authorization", auth);
-//        NameValuePair userName = new NameValuePair("username", username);// 邮箱
-//        NameValuePair password = new NameValuePair("password", pwd);// 密码
-//        NameValuePair grantType = new NameValuePair("grant_type", "password");// 密码
-//
-//        NameValuePair[] data = {userName, password, grantType};
-//        login.setRequestBody(data);
-//        httpClient.executeMethod(login);
-//        int statusCode = login.getStatusCode();
-//        if (statusCode == 200) {
-//            String result = login.getResponseBodyAsString();
-//            JSONObject jsonObject = (JSONObject) JSON.parse(result);
-//            String accessToken = (String) jsonObject.get("access_token");
-//            return accessToken;
-//        } else {
-//            return null;
-//        }
-        return null;
+        HttpClientParams httpParams = new HttpClientParams();
+        httpParams.setSoTimeout(30000);
+        httpClient.setParams(httpParams);
+        httpClient.getHostConfiguration().setHost(authServerHost, Integer.valueOf(authServerPort));
+        httpClient.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "UTF-8");
+        PostMethod login = new PostMethod(authServerApi);
+        login.addRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        final String auth = "Basic " + new String(Base64.encodeBase64(new StringBuilder(Constant.AUTHORIZE_USERNAME + ":" + Constant.AUTHORIZE_PASSWORD).toString().getBytes()));
+        login.addRequestHeader("Authorization", auth);
+        NameValuePair userName = new NameValuePair("username", username);// 邮箱
+        NameValuePair password = new NameValuePair("password", pwd);// 密码
+        NameValuePair grantType = new NameValuePair("grant_type", "password");// 密码
+
+        NameValuePair[] data = {userName, password, grantType};
+        login.setRequestBody(data);
+        httpClient.executeMethod(login);
+        int statusCode = login.getStatusCode();
+        if (statusCode == 200) {
+            String result = login.getResponseBodyAsString();
+            JSONObject jsonObject = (JSONObject) JSON.parse(result);
+            String accessToken = (String) jsonObject.get("access_token");
+            return accessToken;
+        } else {
+            return null;
+        }
     }
 
     private UserDTO entityToDto(User user) {
