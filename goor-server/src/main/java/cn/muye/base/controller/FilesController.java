e3cb29fdaa7eaeeb75cedeeadbc988a91a141996
Jelynn <jelynn.tang@mrobot.cn>
2017-07-18 17:42:56 +0800

    地图信息添加地图压缩包关联

--- goor-server/src/main/java/cn/muye/base/controller/FilesController.java
+++ goor-server/src/main/java/cn/muye/base/controller/FilesController.java
@@ -301,14 +301,16 @@ public class FilesController {
                 ZipUtils.unzip(mapZip.getFilePath(), saveFile.getAbsolutePath(), false);
                 LOGGER.info("地图文件解压成功，保存地址 path=" + saveFile.getAbsolutePath());
                 //解析地图文件
-                analysisFile(saveFile, mapZip.getDeviceId());
+                analysisFile(saveFile, mapZip);
             } catch (Exception e) {
                 LOGGER.error("地图文件解压失败", e);
             }
         }
     }
 
-    public void analysisFile(File mapFilePath, String deviceId) {
+    public void analysisFile(File mapFilePath, MapZip mapZip) {
+        String deviceId = mapZip.getDeviceId();
+        Long mapzipId = mapZip.getId();
         LOGGER.info("解析导航目标点文件，地图文件地址=" + mapFilePath);
         if (!mapFilePath.exists()) {
             return;
@@ -320,7 +322,7 @@ public class FilesController {
             String sceneName = sceneDir.getName();
             try {
                 //解析地图文件
-                analysisMapFile(sceneName, sceneDir.getAbsolutePath(), deviceId);
+                analysisMapFile(sceneName, sceneDir.getAbsolutePath(), deviceId, mapzipId);
                 //解析导航点文件
                 analysisPointFile(sceneName, sceneDir.getAbsolutePath());
             } catch (IllegalAccessException e) {
@@ -330,7 +332,7 @@ public class FilesController {
     }
 
     //解析地图文件
-    private void analysisMapFile(String sceneName, String sceneDir, String deviceId) throws IllegalAccessException {
+    private void analysisMapFile(String sceneName, String sceneDir, String deviceId, Long mapzipId) throws IllegalAccessException {
         File mapFilePath = FileUtils.getFile(sceneDir, Constant.MAP_FILE_PATH);
         if (!mapFilePath.exists()) {
             LOGGER.info("地图文件夹不存在。sceneName=" + sceneName + ", sceneDir=" + sceneDir);
@@ -350,6 +352,7 @@ public class FilesController {
             MapInfo mapInfo = new MapInfo();
             mapInfo.setMapName(mapName);
             mapInfo.setSceneName(sceneName);
+            mapInfo.setMapZipId(mapzipId);
             mapInfo.setDeviceId(deviceId);
             mapInfo.setCreateTime(new Date());
             //TODO 删除原数据库中的地图数据
