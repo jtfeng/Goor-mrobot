364f7540779782c0874ca06ebaaaedcf467881ca
abel-mi-book <abeldeng@qq.com>
2017-07-18 13:55:45 +0800

    查询任务状态response处理代码提交

---
+++ goor-server/src/main/java/cn/muye/base/consumer/service/X86MissionStateResponseService.java
@@ -0,0 +1,11 @@
+package cn.muye.base.consumer.service;
+
+import cn.muye.base.bean.MessageInfo;
+
+/**
+ * Created by abel on 17-7-11.
+ */
+public interface X86MissionStateResponseService {
+
+    void handleX86MissionStateResponse(MessageInfo messageInfo);
+}
