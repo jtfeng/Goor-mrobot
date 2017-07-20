329187df71919e30a43726f505ecf1168d17a6df
wlkfec <wlkfec@gmail.com>
2017-07-20 16:26:00 +0800

    场景管理基本后台 （第三次提交）

--- goor-server/src/main/java/cn/muye/assets/scene/mapper/SceneMapper.java
+++ goor-server/src/main/java/cn/muye/assets/scene/mapper/SceneMapper.java
@@ -1,7 +1,51 @@
 package cn.muye.assets.scene.mapper;
 
+import cn.mrobot.bean.area.map.MapInfo;
+import cn.mrobot.bean.assets.robot.Robot;
 import cn.mrobot.bean.assets.scene.Scene;
 import cn.muye.util.MyMapper;
 
+import java.util.List;
+
 public interface SceneMapper extends MyMapper<Scene> {
+
+    /**
+     * 保存场景与地图之间的多对多关系数据
+     * @param sceneId
+     * @param mapIds
+     * @return
+     */
+    int insertSceneAndMapRelations(Long sceneId, List<Long> mapIds);
+
+    /**
+     * 保存场景与机器人之间的多对多关系
+     * @param sceneId
+     * @param robotIds
+     * @return
+     */
+    int insertSceneAndRobotRelations(Long sceneId, List<Long> robotIds);
+
+    /**
+     * 根据 ID 查询所有关联的机器人 ID 编号
+     * @param sceneId
+     * @return
+     */
+    List<Robot> findRobotBySceneId(Long sceneId);
+
+    /**
+     * 根据 ID 查询所有关联的地图 ID 编号
+     * @param sceneId
+     * @return
+     */
+    List<MapInfo> findMapBySceneId(Long sceneId);
+
+    /**
+     * 删除历史数据（场景与机器人的对应关系）
+     */
+    void deleteRobotAndSceneRelations(Long sceneId);
+
+    /**
+     * 删除历史数据（场景与地图的对应关系）
+     */
+    void deleteMapAndSceneRelations(Long sceneId);
 }
--- goor-server/src/main/java/cn/muye/assets/scene/service/SceneService.java
+++ goor-server/src/main/java/cn/muye/assets/scene/service/SceneService.java
@@ -23,4 +23,34 @@ public interface SceneService extends BaseService<Scene> {
     int deleteById(Long id);
 
     List<Scene> listScenes(WhereRequest whereRequest);
+
+    Scene updateAliasName(Long sceneId, String aliasName);
+
+    /**
+     * 保存场景与地图之间的多对多关系数据
+     * @param sceneId
+     * @param mapIds
+     * @return
+     */
+    int insertSceneAndMapRelations(Long sceneId, List<Long> mapIds);
+
+    /**
+     * 保存场景与机器人之间的多对多关系
+     * @param sceneId
+     * @param robotIds
+     * @return
+     */
+    int insertSceneAndRobotRelations(Long sceneId, List<Long> robotIds);
+
+    void sendSyncMapMessageToRobots(Long sceneId);
+
+    /**
+     * 删除历史数据（场景与机器人的对应关系）
+     */
+    void deleteRobotAndSceneRelations(Long sceneId);
+
+    /**
+     * 删除历史数据（场景与地图的对应关系）
+     */
+    void deleteMapAndSceneRelations(Long sceneId);
 }
