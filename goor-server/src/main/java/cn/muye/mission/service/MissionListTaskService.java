820a704838a70fc3ab71634d8c56aac0a025e174
abel-mi-book <abeldeng@qq.com>
2017-07-21 09:10:13 +0800

    获取指定场景任务列表接口，存储上报事件完善。任务列表创建时，新增场景id字段。

--- goor-server/src/main/java/cn/muye/mission/service/MissionListTaskService.java
+++ goor-server/src/main/java/cn/muye/mission/service/MissionListTaskService.java
@@ -1,6 +1,7 @@
 package cn.muye.mission.service;
 
 import cn.mrobot.bean.mission.task.MissionListTask;
+import cn.mrobot.utils.WhereRequest;
 import cn.muye.base.service.BaseService;
 
 import java.util.List;
@@ -12,4 +13,16 @@ public interface MissionListTaskService extends BaseService<MissionListTask> {
     List<MissionListTask> findByRobotCodeAndState(
             String robotCode,
             String state);
+
+    MissionListTask findLastByRobotCode(
+            String robotCode);
+
+    List<MissionListTask> listPageByStoreIdAndOrder(
+            int page,
+            int pageSize,
+            Long sceneId,
+            String state,
+            String order);
+
+    List<MissionListTask> tasksList(WhereRequest whereRequest);
 }
--- goor-server/src/main/java/cn/muye/mission/service/MissionListTaskServiceImpl.java
+++ goor-server/src/main/java/cn/muye/mission/service/MissionListTaskServiceImpl.java
@@ -2,7 +2,12 @@ package cn.muye.mission.service;
 
 import cn.mrobot.bean.mission.task.MissionListTask;
 import cn.mrobot.utils.StringUtil;
+import cn.mrobot.utils.WhereRequest;
+import cn.muye.base.bean.SearchConstants;
 import cn.muye.base.service.imp.BaseServiceImpl;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
@@ -17,6 +22,13 @@ import java.util.List;
 public class MissionListTaskServiceImpl
         extends BaseServiceImpl<MissionListTask>
         implements MissionListTaskService {
+
+    @Autowired
+    MissionTaskService missionTaskService;
+
+    @Autowired
+    MissionItemTaskService missionItemTaskService;
+
     @Override
     public List<MissionListTask> findByRobotCodeAndState(
             String robotCode,
@@ -30,5 +42,67 @@ public class MissionListTaskServiceImpl
                 .andCondition("ROBOT_CODE=", robotCode)
                 .andCondition("STATE=", state);
         return myMapper.selectByExample(example);
+    }
+
+    @Override
+    public MissionListTask findLastByRobotCode(String robotCode) {
+        if (StringUtil.isEmpty(robotCode)){
+            return null;
+        }
+        Example example = new Example(MissionListTask.class);
+        example.createCriteria()
+                .andCondition("ROBOT_CODE=", robotCode);
+        example.setOrderByClause("CREATE_TIME DESC");
+        List<MissionListTask> tasks = myMapper.selectByExample(example);
+        if (tasks != null && !tasks.isEmpty()){
+            return tasks.get(0);
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public List<MissionListTask> listPageByStoreIdAndOrder(
+            int page,
+            int pageSize,
+            Long sceneId,
+            String state,
+            String order) {
+        PageHelper.startPage(page, pageSize);
+        Example example = new Example(MissionListTask.class);
+        Example.Criteria criteria = example.createCriteria()
+                .andCondition("SCENE_ID =", sceneId)
+                .andCondition("STORE_ID =", SearchConstants.FAKE_MERCHANT_STORE_ID);
+        if (!StringUtil.isNullOrEmpty(state)){
+            criteria.andCondition("STATE =", state);
+        }
+        if(!StringUtil.isNullOrEmpty(order)){
+            example.setOrderByClause(order);
+        }
+        return myMapper.selectByExample(example);
+    }
+
+    @Override
+    public List<MissionListTask> tasksList(WhereRequest whereRequest) {
+        List<MissionListTask> list = null;
+        if (whereRequest == null ||
+                StringUtil.isNullOrEmpty(whereRequest.getQueryObj())){
+            return null;
+        }
+        JSONObject jsonObject = JSONObject.parseObject(whereRequest.getQueryObj());
+        Long sceneId = jsonObject.getLong("sceneId");
+        String state = jsonObject.getString("state");
+        list = listPageByStoreIdAndOrder(
+                whereRequest.getPage(),
+                whereRequest.getPageSize(),
+                sceneId, state, "CREATE_TIME DESC");
+
+        //填充子数据
+        if (list != null){
+            list.forEach(missionListTask -> {
+
+            });
+        }
+        return list;
     }
 }
