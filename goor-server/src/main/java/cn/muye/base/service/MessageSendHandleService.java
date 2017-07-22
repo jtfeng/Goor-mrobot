50736a84bc756fa5a6728a9a38bb64a81f8df35b
enva <enva.liang@clever-m.com>
2017-07-22 18:25:59 +0800

    添加统一发送例子

--- goor-server/src/main/java/cn/muye/base/service/MessageSendHandleService.java
+++ goor-server/src/main/java/cn/muye/base/service/MessageSendHandleService.java
@@ -6,15 +6,16 @@ import cn.muye.base.bean.MessageInfo;
 
 public interface MessageSendHandleService {
 
+    //单消息（命令）
     AjaxResult sendCommandMessage(boolean toDataBase, boolean x86AgentReply, String robotSN, MessageInfo messageInfo) throws Exception;
-
+    //单消息（资源）
     AjaxResult sendResourceMessage(boolean toDataBase, boolean x86AgentReply, String robotSN, MessageInfo messageInfo) throws Exception;
-
+    //单消息（x86Agent）
     AjaxResult sendToX86Message(boolean toDataBase, boolean x86AgentReply, String robotSN, MessageInfo messageInfo) throws Exception;
-
+    //命令发送全部机器人
     AjaxResult sendCommandMessageAndAll(boolean toDataBase, MessageInfo messageInfo) throws Exception;
-
+    //资源发送全部机器人
     AjaxResult sendResourceMessageAndAll(boolean toDataBase, MessageInfo messageInfo) throws Exception;
-
+    //发送到x86Agent全部机器人
     AjaxResult sendToX86MessageAndAll(boolean toDataBase, MessageInfo messageInfo) throws Exception;
 }
