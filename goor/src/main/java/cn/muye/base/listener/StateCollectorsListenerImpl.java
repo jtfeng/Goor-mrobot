b3342e64e0785b7062c7c089d2ee77871d7a2825
Jelynn <jelynn.tang@mrobot.cn>
2017-07-24 20:32:33 +0800

    修改上传云端频率

--- goor/src/main/java/cn/muye/base/listener/StateCollectorsListenerImpl.java
+++ goor/src/main/java/cn/muye/base/listener/StateCollectorsListenerImpl.java
@@ -1,9 +1,13 @@
 package cn.muye.base.listener;
 
 import cn.mrobot.bean.constant.TopicConstants;
+import cn.mrobot.bean.state.StateCollectorResponse;
+import cn.mrobot.utils.StringUtil;
 import cn.muye.base.bean.SingleFactory;
-import cn.muye.base.bean.TopicSubscribeInfo;
+import cn.muye.base.cache.CacheInfoManager;
 import cn.muye.base.producer.ProducerCommon;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import edu.wpi.rail.jrosbridge.callback.TopicCallback;
 import edu.wpi.rail.jrosbridge.messages.Message;
 import org.apache.log4j.Logger;
@@ -14,7 +18,8 @@ import org.apache.log4j.Logger;
  * User: Chay
  * Date: 2017/6/14
  * Time: 14:23
- * Describe:
+ * Describe: 目前状态机上报为每秒钟20次。所以需要更改向云端上报的频率。添加计数器，没2秒上报一次
+ * 根据状态的module 放入缓存，缓存的存活时间为2秒，每次去缓存中检测，存在对应的module。则上传
  * Version:1.0
  */
 public class StateCollectorsListenerImpl implements TopicCallback {
@@ -24,9 +29,15 @@ public class StateCollectorsListenerImpl implements TopicCallback {
     public void handleMessage(Message message) {
         if (TopicConstants.DEBUG)
             logger.info("From ROS ====== state_collectors topic  " + message.toString());
-        if (TopicConstants.DEBUG)
-            logger.info(" ====== message.toString()===" + message.toString());
-        ProducerCommon msg = SingleFactory.getProducerCommon();
-        msg.sendStateCollectorMessage(message.toString());
+        if (!StringUtil.isNullOrEmpty(message.toString())) {
+            JSONObject jsonObject = JSON.parseObject(message.toString());
+            String data = jsonObject.getString(TopicConstants.DATA);
+            StateCollectorResponse stateCollectorResponse = JSON.parseObject(data, StateCollectorResponse.class);
+            if(!CacheInfoManager.getStateModuleCache(stateCollectorResponse.getModule())){
+                //往云端推送
+                ProducerCommon msg = SingleFactory.getProducerCommon();
+                msg.sendStateCollectorMessage(message.toString());
+            }
+        }
     }
 }
