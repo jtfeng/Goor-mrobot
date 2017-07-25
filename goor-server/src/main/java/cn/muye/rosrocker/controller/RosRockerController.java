6b9c3b880b4c86e4a2261379f31dae0f985865ba
wlkfec <wlkfec@gmail.com>
2017-07-25 16:32:01 +0800

    场景管理基本后台 （第十一次提交）

--- goor-server/src/main/java/cn/muye/rosrocker/controller/RosRockerController.java
+++ goor-server/src/main/java/cn/muye/rosrocker/controller/RosRockerController.java
@@ -11,12 +11,10 @@ import cn.muye.base.bean.MessageInfo;
 import cn.muye.base.bean.RabbitMqBean;
 import cn.muye.rosrocker.bean.RosRockerPubBean;
 import com.alibaba.fastjson.JSON;
+import static com.google.common.base.Preconditions.*;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -50,6 +48,31 @@ public class RosRockerController {
             return AjaxResult.failed(e.getMessage());
         }
     }
+
+    /**
+     * 通过指定命令 控制机器人或者任务 执行指定的不同操作（包含：pause、resume、skipMission、skipMissionList、clear）
+     * @param robotId
+     * @param command
+     * @return
+     */
+    @RequestMapping(value = "/controlState/{robotId}/{command}", method = RequestMethod.GET)
+    public Object controlState(@PathVariable("robotId") String robotId, @PathVariable("command") String command){
+        try {
+            checkArgument(!("".equals(robotId.trim())), "机器人编号不能为空串!");
+            checkArgument(!("".equals(robotId.trim())), "传入的指令不能为空串!");
+            //分别根据不同的指令内容，执行不同的操作。
+            this.sendDetailMessage(robotId,
+                    TopicConstants.X86_MISSION_INSTANT_CONTROL,
+                    TopicConstants.TOPIC_TYPE_STRING,
+                    JSON.toJSONString(new HashMap<String, String>(){{
+                        put("command", command);
+                    }}));
+            return AjaxResult.success("指令调用成功");
+        }catch (Exception e){
+            return AjaxResult.failed( "指令调用失败");
+        }
+    }
+
     /**
      * 启动控制
      * @return
