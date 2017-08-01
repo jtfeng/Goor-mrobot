d746c74762d5cdfc3a3503e195016807302bf603
enva <enva.liang@clever-m.com>
2017-08-01 20:09:52 +0800

    限制每2s发送一次位置信息

--- goor/src/main/java/cn/muye/base/listener/CurrentPoseListenerImpl.java
+++ goor/src/main/java/cn/muye/base/listener/CurrentPoseListenerImpl.java
@@ -2,6 +2,7 @@ package cn.muye.base.listener;
 
 import cn.mrobot.bean.constant.TopicConstants;
 import cn.muye.base.bean.SingleFactory;
+import cn.muye.base.cache.CacheInfoManager;
 import cn.muye.base.producer.ProducerCommon;
 import edu.wpi.rail.jrosbridge.callback.TopicCallback;
 import edu.wpi.rail.jrosbridge.messages.Message;
@@ -14,8 +15,11 @@ public class CurrentPoseListenerImpl implements TopicCallback{
 		try {
             if (TopicConstants.DEBUG)
 			logger.info("From ROS ====== CurrentPose topic  " + message.toString());
-			ProducerCommon msg = SingleFactory.getProducerCommon();
-			msg.sendCurrentPoseMessage(message.toString());
+            if((System.currentTimeMillis() - CacheInfoManager.getCurrentPoseSendTime()) > 2000){//每两秒发送一次位置消息
+				CacheInfoManager.setCurrentPoseSendTime();
+				ProducerCommon msg = SingleFactory.getProducerCommon();
+				msg.sendCurrentPoseMessage(message.toString());
+			}
 		}catch (Exception e){
 			logger.error("CurrentPoseListenerImpl error",e);
 		}
