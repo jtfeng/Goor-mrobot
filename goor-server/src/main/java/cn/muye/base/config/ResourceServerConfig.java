a16d77177e7b95dda111e9dcd94d5b848be9e912
ray.fu <ray.fu@mrobot.cn>
2017-08-03 10:25:52 +0800

    去掉登录校验

--- goor-server/src/main/java/cn/muye/base/config/ResourceServerConfig.java
+++ goor-server/src/main/java/cn/muye/base/config/ResourceServerConfig.java
@@ -11,16 +11,16 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/account/user/login").permitAll()
-                .antMatchers("/account/user/login/pad").permitAll()
-                .antMatchers("/account/user/logOut").permitAll()
-                .antMatchers("/resources/static/**").permitAll()
-                .antMatchers("/services/**").permitAll()
-                .antMatchers("/check**").permitAll()
-                .antMatchers("/druid/**").permitAll()
+//        http.authorizeRequests()
+//                .antMatchers("/account/user/login").permitAll()
+//                .antMatchers("/account/user/login/pad").permitAll()
+//                .antMatchers("/account/user/logOut").permitAll()
+//                .antMatchers("/resources/static/**").permitAll()
+//                .antMatchers("/services/**").permitAll()
+//                .antMatchers("/check**").permitAll()
+//                .antMatchers("/druid/**").permitAll()
 //                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated();
+        http.authorizeRequests().anyRequest().permitAll();
     }
 
 }
