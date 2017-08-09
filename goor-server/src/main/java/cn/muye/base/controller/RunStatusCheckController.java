e582b429f8aa53914dc853ede7f2a433ea42133a
enva <enva.liang@clever-m.com>
2017-08-09 19:32:20 +0800

    添加mq

--- goor-server/src/main/java/cn/muye/base/controller/RunStatusCheckController.java
+++ goor-server/src/main/java/cn/muye/base/controller/RunStatusCheckController.java
@@ -16,18 +16,27 @@ public class RunStatusCheckController {
 
 	@RequestMapping(value = "checkOnLine")
 	@ResponseBody
-	public String checkOnLine(HttpServletRequest request) {
+	public int checkOnLine(HttpServletRequest request) {
 		log.info("goor-server运行正常");
-		return "1";
+		return 1;
 	}
 
 	@RequestMapping(value = "checkRosBridge")
 	@ResponseBody
-	public String checkRosBridge(HttpServletRequest request) {
+	public int checkRosBridge(HttpServletRequest request) {
 //		if ((System.currentTimeMillis() - CacheInfoManager.getTopicHeartCheckCache()) > TopicConstants.CHECK_ROS_BRIDGE_MAX) {
 //			return "1";
 //		}
-		return "1";
+		return 1;
+	}
+
+	@RequestMapping(value = "checkRosBridge111")
+	@ResponseBody
+	public int checkRosBridge111(HttpServletRequest request) {
+//		if ((System.currentTimeMillis() - CacheInfoManager.getTopicHeartCheckCache()) > TopicConstants.CHECK_ROS_BRIDGE_MAX) {
+//			return "1";
+//		}
+		return 0;
 	}
 
 }
