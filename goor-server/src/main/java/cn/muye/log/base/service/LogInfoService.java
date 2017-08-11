c3742f45278fcd9776bf608a4dc735f357495ad2
Jelynn <jelynn.tang@mrobot.cn>
2017-08-11 15:07:05 +0800

    添加
    (cherry picked from commit 349ad01)

--- goor-server/src/main/java/cn/muye/log/base/service/LogInfoService.java
+++ goor-server/src/main/java/cn/muye/log/base/service/LogInfoService.java
@@ -2,15 +2,7 @@ package cn.muye.log.base.service;
 
 import cn.mrobot.bean.log.LogInfo;
 import cn.mrobot.utils.WhereRequest;
-import cn.muye.base.bean.SearchConstants;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import cn.muye.log.base.mapper.LogInfoMapper;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,4 +21,6 @@ public interface LogInfoService {
     void save(LogInfo logInfo);
 
     List<LogInfo> lists(WhereRequest whereRequest, Long storeId);
+
+    int update(LogInfo logInfo);
 }
