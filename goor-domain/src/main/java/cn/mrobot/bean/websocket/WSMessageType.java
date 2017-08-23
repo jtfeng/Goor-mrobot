8bcf3ad9782a397d8c00886d101ef8015512128a
Jelynn <jelynn.tang@mrobot.cn>
2017-08-23 15:06:33 +0800

    添加websocket发送当前位置信息和低电量警告

--- goor-domain/src/main/java/cn/mrobot/bean/websocket/WSMessageType.java
+++ goor-domain/src/main/java/cn/mrobot/bean/websocket/WSMessageType.java
@@ -4,6 +4,7 @@ package cn.mrobot.bean.websocket;
  * Created by Jelynn on 2017/8/17.
  */
 public enum  WSMessageType {
+    REGISTER,
     NOTIFICATION,
     WARNING,
     POSE,
--- goor-server/src/main/java/cn/muye/base/websoket/WebSocketSendMessage.java
+++ goor-server/src/main/java/cn/muye/base/websoket/WebSocketSendMessage.java
@@ -1,50 +1,73 @@
 package cn.muye.base.websoket;
 
-import cn.mrobot.utils.StringUtil;
+import cn.mrobot.bean.websocket.WSMessage;
 import cn.muye.base.cache.CacheInfoManager;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.thymeleaf.util.StringUtils;
 
-import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by enva on 17/07/20.
  */
-@Component
 @Slf4j
+@Component
 public class WebSocketSendMessage {
 
     /**
      * webSocket发送消息方法，向web页面推送消息
-     * @param userName
-     * @param sendMessage
+     *
+     * @param wsMessage
      * @return
      * @throws Exception
      */
-    public boolean sendWebSocketMessage(String userName, String sendMessage) throws Exception {
-        if(StringUtils.isEmpty(userName) || StringUtil.isEmpty(sendMessage)){
-            return false;
-        }
-        Session session = CacheInfoManager.getWebSocketSessionCache(userName);
-        if(null != session){
-            session.getBasicRemote().sendText(sendMessage);
-            return true;
+    public boolean sendWebSocketMessage(WSMessage wsMessage) throws Exception {
+        String userId = wsMessage.getUserId();
+        Session session = CacheInfoManager.getWebSocketSessionCache(userId);
+        if (null != session) {
+            sendMessage(session, JSON.toJSONString(wsMessage));
+        } else {
+            sendAll(JSON.toJSONString(wsMessage));
         }
         return false;
+    }
 
+    private void sendMessage(Session session, String message) {
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException e) {
+            log.error("websocket 发送消息出错", e);
+        }
     }
 
     /**
-     * 返回webSocket连接在线数
-     * @return
+     * 向所有用户发送
+     *
+     * @param message
      */
-    public int getWebSocketOnline(){
-        return CacheInfoManager.getWebSocketSessionCacheSize();
+    private void sendAll(String message) {
+        Map<String, Session> sessionMap = CacheInfoManager.getWebSocketSessionCache();
+        Iterator iterator = sessionMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Session session = null;
+            try {
+                Map.Entry<String, Session> entry = (Map.Entry<String, Session>) iterator.next();
+                session = entry.getValue();
+                sendMessage(session, message);
+            } catch (Exception e) {
+                log.debug("群发消息异常", e);
+                CacheInfoManager.removeWebSocketSessionCache(session);
+                try {
+                    session.close();
+                } catch (IOException e1) {
+                    log.error("websocket session 关闭异常", e);
+                }
+            }
+        }
     }
 
 }
