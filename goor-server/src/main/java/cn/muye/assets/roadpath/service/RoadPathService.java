009d053aaea52e52da04517d02255ad625864e1b
wlkfec <wlkfec@gmail.com>
2017-09-05 09:25:06 +0800

    修改路径 Service 方法

--- goor-server/src/main/java/cn/muye/assets/roadpath/service/RoadPathService.java
+++ goor-server/src/main/java/cn/muye/assets/roadpath/service/RoadPathService.java
@@ -2,6 +2,7 @@ package cn.muye.assets.roadpath.service;
 
 import cn.mrobot.bean.assets.roadpath.RoadPath;
 import cn.mrobot.bean.assets.roadpath.RoadPathDetail;
+import cn.mrobot.utils.WhereRequest;
 import cn.muye.base.service.BaseService;
 
 import java.util.List;
@@ -13,4 +14,11 @@ public interface RoadPathService extends BaseService<RoadPath> {
 
     List<RoadPathDetail> findRoadPathByStartAndEndPoint(Long startPoint, Long endPoint, String sceneName, String mapName) throws Exception;
 
+    void updateRoadPath(Map<String, Object> body) throws Exception;
+
+    List<RoadPathDetail> listRoadPathDetailByStartAndEndPointType(Long startPoint, Long endPoint, String sceneName, String mapName, Integer pathType) throws Exception;
+
+    List<RoadPath> listRoadPathByStartAndEndPoint(Long startPoint, Long endPoint, String sceneName, String mapName, Integer pathType) throws Exception;
+
+    List<RoadPathDetail> listRoadPaths(WhereRequest whereRequest) throws Exception;
 }
