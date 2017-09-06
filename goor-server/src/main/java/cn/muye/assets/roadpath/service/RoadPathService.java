1d9092d3c98984c4f350b1ad95730facc86672c5
wlkfec <wlkfec@gmail.com>
2017-09-06 14:39:12 +0800

    路径信息相关代码修改

--- goor-server/src/main/java/cn/muye/assets/roadpath/service/RoadPathService.java
+++ goor-server/src/main/java/cn/muye/assets/roadpath/service/RoadPathService.java
@@ -2,6 +2,8 @@ package cn.muye.assets.roadpath.service;
 
 import cn.mrobot.bean.assets.roadpath.RoadPath;
 import cn.mrobot.bean.assets.roadpath.RoadPathDetail;
+import cn.mrobot.bean.assets.scene.Scene;
+import cn.mrobot.utils.WhereRequest;
 import cn.muye.base.service.BaseService;
 
 import java.util.List;
@@ -11,6 +13,14 @@ public interface RoadPathService extends BaseService<RoadPath> {
 
     void createRoadPath(Map<String, Object> body) throws Exception;
 
+    void updateRoadPath(Map<String, Object> body) throws Exception;
+
     List<RoadPathDetail> findRoadPathByStartAndEndPoint(Long startPoint, Long endPoint, String sceneName, String mapName) throws Exception;
+
+    List<RoadPathDetail> listRoadPathDetailByStartAndEndPointType(Long startPoint, Long endPoint, String sceneName, String mapName, Integer pathType) throws Exception;
+
+    List<RoadPath> listRoadPathByStartAndEndPoint(Long startPoint, Long endPoint, String sceneName, String mapName, Integer pathType) throws Exception;
+
+    List<RoadPathDetail> listRoadPaths(WhereRequest whereRequest) throws Exception;
 
 }
