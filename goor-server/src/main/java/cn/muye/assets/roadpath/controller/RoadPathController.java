81dc889854afe73f99d74a9eaf90df6f705a2768
wlkfec <wlkfec@gmail.com>
2017-09-06 14:41:56 +0800

    路径信息相关代码修改

--- goor-server/src/main/java/cn/muye/assets/roadpath/controller/RoadPathController.java
+++ goor-server/src/main/java/cn/muye/assets/roadpath/controller/RoadPathController.java
@@ -3,7 +3,10 @@ package cn.muye.assets.roadpath.controller;
 import cn.mrobot.bean.AjaxResult;
 import cn.mrobot.bean.assets.roadpath.RoadPath;
 import cn.mrobot.bean.assets.roadpath.RoadPathDetail;
+import cn.mrobot.bean.assets.scene.Scene;
+import cn.mrobot.utils.WhereRequest;
 import cn.muye.assets.roadpath.service.RoadPathService;
+import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +41,59 @@ public class RoadPathController {
             return AjaxResult.success();
         }catch (Exception e){
             return AjaxResult.failed(e.getMessage());
+        }
+    }
+
+    /**
+     * 更新
+     * @param body
+     * @return
+     */
+    @RequestMapping(value = "/asset/roadPath", method = RequestMethod.PUT)
+    public AjaxResult updateRoadPath(@RequestBody Map<String, Object> body){
+        // pathId 路径编号信息
+        // 拟合方式现假定有三种 （1：直线；2：曲线；3.云端定义）
+        // String -> pathName
+        // String -> pattern
+        // String -> data
+        // List<Long> -> points -> 前端传递格式为 一个数组
+        // Long cloudSceneId
+        try {
+//            roadPathService.createRoadPath(body);
+            return AjaxResult.success();
+        }catch (Exception e){
+            return AjaxResult.failed(e.getMessage());
+        }
+    }
+
+    /**
+     * 删除指定的道路路径编号
+     * @param id
+     * @return
+     */
+    @ RequestMapping(value = "/asset/roadPath/{id}", method = RequestMethod.DELETE)
+    public AjaxResult deleteRoadPath(@PathVariable("id") String id){
+        try {
+            this.roadPathService.deleteById(Long.parseLong(id));
+            return AjaxResult.success();
+        }catch (Exception e){
+            return AjaxResult.failed(e.getMessage());
+        }
+    }
+
+    /**
+     * 分页列表查询路径信息
+     * @param whereRequest
+     * @return
+     */
+    @GetMapping("/asset/roadPath")
+    public AjaxResult roadPathList(WhereRequest whereRequest) {
+        try {
+            List<RoadPathDetail> list = roadPathService.listRoadPaths(whereRequest);
+            PageInfo<RoadPathDetail> pageList = new PageInfo<>(list);
+            return AjaxResult.success(pageList, "查询成功");
+        } catch (Exception e) {
+            return AjaxResult.failed(e,         "查询失败");
         }
     }
 
--- goor-server/src/main/java/cn/muye/assets/roadpath/service/RoadPathService.java
+++ goor-server/src/main/java/cn/muye/assets/roadpath/service/RoadPathService.java
@@ -2,6 +2,7 @@ package cn.muye.assets.roadpath.service;
 
 import cn.mrobot.bean.assets.roadpath.RoadPath;
 import cn.mrobot.bean.assets.roadpath.RoadPathDetail;
+import cn.mrobot.bean.assets.scene.Scene;
 import cn.mrobot.utils.WhereRequest;
 import cn.muye.base.service.BaseService;
 
@@ -12,13 +13,14 @@ public interface RoadPathService extends BaseService<RoadPath> {
 
     void createRoadPath(Map<String, Object> body) throws Exception;
 
-    List<RoadPathDetail> findRoadPathByStartAndEndPoint(Long startPoint, Long endPoint, String sceneName, String mapName) throws Exception;
-
     void updateRoadPath(Map<String, Object> body) throws Exception;
+
+    List<RoadPathDetail> findRoadPathByStartAndEndPoint(Long startPoint, Long endPoint, String sceneName, String mapName) throws Exception;
 
     List<RoadPathDetail> listRoadPathDetailByStartAndEndPointType(Long startPoint, Long endPoint, String sceneName, String mapName, Integer pathType) throws Exception;
 
     List<RoadPath> listRoadPathByStartAndEndPoint(Long startPoint, Long endPoint, String sceneName, String mapName, Integer pathType) throws Exception;
 
     List<RoadPathDetail> listRoadPaths(WhereRequest whereRequest) throws Exception;
+
 }
