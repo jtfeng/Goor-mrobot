9da898b0577de4bb0ab0d153a489c973f67da7bf
Jelynn <jelynn.tang@mrobot.cn>
2018-01-12 18:25:16 +0800

    修改版本号

--- goor-domain/src/main/java/cn/mrobot/bean/constant/VersionConstants.java
+++ goor-domain/src/main/java/cn/mrobot/bean/constant/VersionConstants.java
@@ -9,7 +9,7 @@ public class VersionConstants {
 
     //版本信息
     public final static String VERSION_NOAH_GOOR_SERVER_KEY = "goorServerVersion";//服务端键名
-    public final static String VERSION_NOAH_GOOR_SERVER = "NOAH_GoorServer_Ver0.0.0.1013_test";//服务端：NOAH_GoorServer_Ver0.0.0.1012_test
+    public final static String VERSION_NOAH_GOOR_SERVER = "NOAH_GoorServer_Ver0.0.0.1014_test";//服务端：NOAH_GoorServer_Ver0.0.0.1012_test
     public final static String VERSION_NOAH_GOOR_KEY = "goor_version";//agent端键名
 //    public final static String VERSION_NOAH_GOOR = "NOAH_Goor_Ver0.0.0.1007_test";//agent端：NOAH_Goor_Ver0.0.0.1007_test
     //20180110暂时只能用数字
