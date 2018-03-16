2999aecb0027cf8751c1d60cceff6f851f77e266
Jelynn <jelynn.tang@mrobot.cn>
2018-03-16 14:10:01 +0800

    实体类提交

--- goor-domain/src/main/java/cn/mrobot/bean/assets/elevator/ElevatorNotice.java
+++ goor-domain/src/main/java/cn/mrobot/bean/assets/elevator/ElevatorNotice.java
@@ -1,14 +1,13 @@
 package cn.mrobot.bean.assets.elevator;
 
-import cn.mrobot.bean.area.station.Station;
-import cn.mrobot.bean.assets.good.GoodsType;
 import cn.mrobot.bean.base.BaseBean;
-import cn.mrobot.bean.erp.order.OperationOrder;
 
 import javax.persistence.Table;
 import java.util.Date;
 
 /**
+ * 消息通知的实体类，通过type区分是什么消息通知，
+ *
  * @author Jelynn
  * @date 2018/1/8
  */
@@ -25,6 +24,8 @@ public class ElevatorNotice extends BaseBean {
 
     private int state; //状态:0 :初始状态，1：pad接收到消息并反馈
 
+    private int type; //消息类型 1:电梯pad消息  2:多站到达消息
+
     private String robotCode;
 
     private Long toStationId;
@@ -32,6 +33,10 @@ public class ElevatorNotice extends BaseBean {
     private String fromStationName; //发货站名称
 
     private String goodsTypeName; //物品类型名称
+
+    private String data; //其他数据，JSON格式的字符串，到站消息为物品信息的数据
+
+    private Long orderDetailId;   //订单细节id
 
     public Integer getCallFloor() {
         return callFloor;
@@ -105,10 +110,52 @@ public class ElevatorNotice extends BaseBean {
         this.goodsTypeName = goodsTypeName;
     }
 
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Long getOrderDetailId() {
+        return orderDetailId;
+    }
+
+    public void setOrderDetailId(Long orderDetailId) {
+        this.orderDetailId = orderDetailId;
+    }
+
     public ElevatorNotice init() {
         this.setState(State.INIT.getCode());
         this.setCreateTime(new Date());
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ElevatorNotice that = (ElevatorNotice) o;
+
+        if (!uuid.equals(that.uuid)) return false;
+        return toStationId.equals(that.toStationId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = uuid.hashCode();
+        result = 31 * result + toStationId.hashCode();
+        return result;
     }
 
     public enum State {
@@ -125,4 +172,20 @@ public class ElevatorNotice extends BaseBean {
             return code;
         }
     }
+
+    public enum Type {
+        ELEVATOR_NOTICE(1),
+        ARRIVAL_STATION_NOTICE(2);
+
+        private int code;
+
+        private Type(int code) {
+            this.code = code;
+        }
+
+        public int getCode() {
+            return code;
+        }
+    }
+
 }
